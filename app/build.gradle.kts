01e5a743b2fe2a99f96033457333d5e039bfbe32
student9128 <jingpengchn@163.com>
2024-11-18 14:56:11 +0800

    upgrade:upgrade libs version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.kevin.composestudy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kevin.composestudy"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
